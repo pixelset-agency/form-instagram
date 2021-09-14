92282ba43dcf2e5c86b9fa8fba2e3c67392c635e
Pixel Set <pixelset.me@gmail.com>
2021-09-14 00:05:54 -0300

    Formulário Instagram

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const name = document.querySelector("#name");
+
+    const value = name.value;
+
+    console.log(value);
+
+});
